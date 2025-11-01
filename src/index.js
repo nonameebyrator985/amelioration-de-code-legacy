767903a3a7b5b3c817f79be73118f43389fc0854
nonameebyrator985 <singheingemo@hotmail.com>
2025-11-01 18:37:47 +0900

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { analyserCode } from './analyser';
+import { refactoriserCode } from './refactor';
+import { genererTests } from './tests';
+
+const demarrerOutil = () => {
+    console.log('Démarrage de l'outil d'amélioration de code hérité');
+    // Exécuter les différentes fonctionnalités ici
+};
+
+demarrerOutil();
