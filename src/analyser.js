fe2072e99f07c7e63b862280fcfd42c7712dd2c5
nonameebyrator985 <singheingemo@hotmail.com>
2025-11-01 18:37:49 +0900

    feat: initial commit for src/analyser.js

---
+++ src/analyser.js
@@ -0,0 +1,6 @@
+export const analyserCode = (code) => {
+    // Logique d'analyse statique du code
+    console.log('Analyse du code...');
+    const problèmes = []; // Logique pour identifier les problèmes
+    return problèmes;
+};
